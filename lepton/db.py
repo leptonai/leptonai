4f32de188315096f8bcfce493f4e12a459bc4451
bddppq <jbai@lepton.ai>
2023-05-03 00:41:26 -0700

    fix(sdk): remove "photons" db table (#16)
    there were duplicated (photon and photons) db tables
    
    Fix #14

--- lepton/db.py
+++ lepton/db.py
@@ -4,6 +4,6 @@ from .config import DB_PATH
 
 DB = sqlite3.connect(DB_PATH)
 DB.cursor().execute(
-    "CREATE TABLE IF NOT EXISTS photons (name TEXT, model TEXT, path TEXT)"
+    "CREATE TABLE IF NOT EXISTS photon (name TEXT, model TEXT, path TEXT)"
 )
 DB.commit()
