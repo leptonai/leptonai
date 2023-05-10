5c65af1724f5c2cba7ca953b98bfaeaca854f48b
Cong Ding <cong@ccding.com>
2023-05-10 15:57:08 -0500

    fix(server): support PATCH in cors (#83)

--- server/main.go
+++ server/main.go
@@ -44,7 +44,7 @@ func main() {
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if c.Request.Method == "OPTIONS" {
