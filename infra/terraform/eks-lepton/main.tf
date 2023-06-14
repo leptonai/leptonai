122bfc3892109a3ce9f9abdf2f8c46e68ff4f6c7
Gyuho Lee <133369823+0xgyuho@users.noreply.github.com>
2023-06-14 10:55:45 -0700

    feat(infra): external-dns with policy=sync and domain filters (#628)
    Signed-off-by: Gyuho Lee <gyuho@lepton.ai>

--- infra/terraform/eks-lepton/main.tf
+++ infra/terraform/eks-lepton/main.tf
@@ -286,6 +286,59 @@ module "eks_blueprints_kubernetes_addons" {
 
   enable_cluster_autoscaler = true
 
+  # https://github.com/kubernetes-sigs/external-dns/releases
+  enable_external_dns = true
+  external_dns = {
+    name = "external-dns"
+
+    # https://github.com/kubernetes-sigs/external-dns/releases
+    chart_version = "1.13.0"
+
+    repository = "https://kubernetes-sigs.github.io/external-dns/"
+    namespace  = "external-dns"
+
+    # https://github.com/aws-ia/terraform-aws-eks-blueprints-addons/blob/main/main.tf
+    create_policy = true
+    create_role   = true
+
+    # https://github.com/kubernetes-sigs/external-dns/blob/master/charts/external-dns/values.yaml
+    # https://github.com/kubernetes-sigs/external-dns/blob/master/pkg/apis/externaldns/types.go
+    values = [yamlencode({
+      # default is "upsert-only" -- DNS records will not get deleted even when the Ingress resources are deleted
+      # "sync" ensures when ingress resource is deleted, the corresponding DNS record in Route53 gets deleted
+      # TODO: these creates a lot of dangling DNS records, use "sync" once we figure out
+      # https://github.com/kubernetes-sigs/external-dns/issues/2674
+      policy : "sync"
+
+      # When using the TXT registry, a name that identifies this instance of ExternalDNS
+      # (default: default)  
+      txtOwnerId : "${module.eks.cluster_name}"
+
+      # Limit possible target zones by a domain suffix; specify multiple times for multiple domains (optional)
+      # make ExternalDNS see only the hosted zones matching provided domain, omit to process all available hosted zones
+      domainFilters : [
+        "${module.eks.cluster_name}.cloud.lepton.ai"
+      ]
+
+      serviceAccount = {
+        create = true
+      }
+      rbac = {
+        create = true
+      }
+    })]
+
+    oidc_providers = {
+      main = {
+        provider_arn               = module.eks.oidc_provider_arn
+        namespace_service_accounts = ["external-dns:external-dns-sa"]
+      }
+    }
+  }
+  external_dns_route53_zone_arns = [
+    "arn:aws:route53:::hostedzone/${var.lepton_cloud_route53_zone_id}"
+  ]
+
   #---------------------------------------------------------------
   # Prometheus Add-on
   #---------------------------------------------------------------
@@ -351,12 +404,4 @@ EOT
       }
     ]
   }
-
-  enable_external_dns = true
-  external_dns = {
-    create_role = true
-  }
-  external_dns_route53_zone_arns = [
-    "arn:aws:route53:::hostedzone/${var.lepton_cloud_route53_zone_id}"
-  ]
 }
