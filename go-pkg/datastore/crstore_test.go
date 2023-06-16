7bf8e7ef571dd5402c95dfc1ae819b1797fa5676
Cong Ding <cong@ccding.com>
2023-06-15 20:06:37 -0500

    fix(go-pkg): run tests create/delete within a ns (#675)

--- go-pkg/datastore/crstore_test.go
+++ go-pkg/datastore/crstore_test.go
@@ -1,54 +1,67 @@
 package datastore
 
 import (
+	"context"
 	"testing"
 
+	"github.com/leptonai/lepton/go-pkg/k8s"
 	"github.com/leptonai/lepton/go-pkg/util"
 	mothershipv1alpha1 "github.com/leptonai/lepton/lepton-mothership/crd/api/v1alpha1"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestCRStore(t *testing.T) {
 	// TODO split the test and work on more test cases
-	namespace := "unit-test"
+	namespace := util.RandString(8)
+	nsObj := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: namespace,
+		},
+	}
+	if err := k8s.Client.Create(context.Background(), nsObj); err != nil {
+		t.Fatalf("Failed to create namespace %s: %v", namespace, err)
+	}
+
 	example := &mothershipv1alpha1.LeptonCluster{}
 	s := NewCRStore[*mothershipv1alpha1.LeptonCluster](namespace, example)
 
-	name := util.RandString(6)
-
-	lcs, err := s.List()
-	if err != nil {
-		t.Fatalf("Failed to list: %v", err)
-	}
-	if len(lcs) != 0 {
-		t.Fatalf("Expected 0 item in list, got %d", len(lcs))
-	}
-
-	if err = s.Create(name, example); err != nil {
+	crName := util.RandString(6)
+	// Create
+	if err := s.Create(crName, example); err != nil {
 		t.Fatalf("Failed to create: %v", err)
 	}
 
 	defer func() {
-		err := s.Delete(name)
-		if err != nil {
-			t.Fatalf("Failed to delete: %v", err)
+		if err := k8s.Client.Delete(context.Background(), nsObj); err != nil {
+			t.Fatalf("Failed to delete namespace %s: %v", namespace, err)
 		}
 	}()
-
-	lc, err := s.Get(name)
+	// Get
+	lc, err := s.Get(crName)
 	if err != nil {
 		t.Fatalf("Failed to get: %v", err)
 	}
-	if lc.GetName() != name {
+	if lc.GetName() != crName {
 		t.Fatalf("Failed to get: %v", err)
 	}
-	lcs, err = s.List()
+	// List
+	lcs, err := s.List()
 	if err != nil {
 		t.Fatalf("Failed to list: %v", err)
 	}
-	if len(lcs) != 1 {
-		t.Fatalf("Expected 1 item in list, got %d", len(lcs))
+	found := false
+	for _, lc := range lcs {
+		if lc.GetName() == crName {
+			found = true
+		}
 	}
-	if lcs[0].GetName() != name {
-		t.Fatalf("Expected name %s, got %s", name, lcs[0].GetName())
+	if !found {
+		t.Fatalf("Expect to find %s in list, but not found", crName)
+	}
+	// Delete
+	if err := s.Delete(crName); err != nil {
+		t.Fatalf("Failed to delete: %v", err)
 	}
 }
