77ba52f3130b859d2dc492d6b853a52fbb4e686a
Gyuho Lee <133369823+0xgyuho@users.noreply.github.com>
2023-06-19 00:30:51 -0700

    fix(infra): use centos based container for nvidia toolkit (#729)
    Signed-off-by: Gyuho Lee <gyuho@lepton.ai>

--- infra/terraform/eks-lepton/nvidia.tf
+++ infra/terraform/eks-lepton/nvidia.tf
@@ -5,5 +5,17 @@ resource "helm_release" "gpu-operator" {
   namespace        = "gpu-operator"
   create_namespace = true
 
+  # https://github.com/NVIDIA/gpu-operator/blob/master/deployments/gpu-operator/values.yaml
+  values = [yamlencode({
+    toolkit = {
+      # this is required for Amazon Linux based AMIs
+      # https://github.com/leptonai/lepton/issues/225
+      # https://github.com/leptonai/lepton/issues/526
+      # https://github.com/NVIDIA/gpu-operator/issues/528
+      # https://catalog.ngc.nvidia.com/orgs/nvidia/teams/k8s/containers/container-toolkit/tags
+      version = "v1.13.1-centos7"
+    }
+  })]
+
   depends_on = [module.eks]
 }
