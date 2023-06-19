515f60ae361f5cc3b9c87f58e2f4dfa37f46d3e1
Cong Ding <cong@ccding.com>
2023-06-18 22:51:31 -0500

    chore(e2e-tests): print more info when fataling monitoring tests (#732)

--- e2e-tests/monitoring_test.go
+++ e2e-tests/monitoring_test.go
@@ -32,20 +32,20 @@ func TestInstanceMonitoring(t *testing.T) {
 	for _, name := range instanceMonitoringInterfaces {
 		instances, err := lepton.Instance().List(mainTestDeploymentID)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("failed to list instances: %s", err)
 		}
 		if len(instances) < 1 {
-			t.Fatal("no instances found")
+			t.Fatalf("no instances found for deployment %s", mainTestDeploymentID)
 		}
 		instanceID := instances[0].ID
 		raw, err := lepton.Monitoring().GetInstanceRaw(name, mainTestDeploymentID, instanceID)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("failed to get instance raw for name %s and instance %s: %s", name, instanceID, err)
 		}
 		s := []interface{}{}
 		err = json.Unmarshal([]byte(raw), &s)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("failed to unmarshal output for name %s and instance %s: %s, with output: %s", name, instanceID, err, raw)
 		}
 	}
 }
@@ -54,12 +54,12 @@ func TestDeploymentMonitoring(t *testing.T) {
 	for _, name := range deploymentMonitoringInterfaces {
 		raw, err := lepton.Monitoring().GetDeploymentRaw(name, mainTestDeploymentID)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("failed to get deployment raw for name %s: %s", name, err)
 		}
 		s := []interface{}{}
 		err = json.Unmarshal([]byte(raw), &s)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("failed to unmarshal output for name %s: %s, with output %s", name, err, raw)
 		}
 	}
 }
