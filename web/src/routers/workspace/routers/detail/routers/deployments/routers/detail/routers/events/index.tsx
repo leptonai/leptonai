bd0567f45b45b7274e2611312dbd0ec91b9a2636
Hsuan Lee <HsuanGM@gmail.com>
2023-06-21 12:34:58 +0800

    feat(web): add timeliness tips for the events (#777)

--- web/src/routers/workspace/routers/detail/routers/deployments/routers/detail/routers/events/index.tsx
+++ web/src/routers/workspace/routers/detail/routers/deployments/routers/detail/routers/events/index.tsx
@@ -8,7 +8,7 @@ import { DeploymentService } from "@lepton-dashboard/routers/workspace/services/
 import { RefreshService } from "@lepton-dashboard/services/refresh.service";
 import { switchMap, tap } from "rxjs";
 import { useStateFromObservable } from "@lepton-libs/hooks/use-state-from-observable";
-import { Table, Tag } from "antd";
+import { ConfigProvider, Empty, Table, Tag } from "antd";
 import { Card } from "@lepton-dashboard/routers/workspace/components/card";
 import dayjs from "dayjs";
 
@@ -31,57 +31,67 @@ export const Events: FC<{ deployment: Deployment }> = ({ deployment }) => {
 
   return (
     <Card shadowless borderless>
-      <Table
-        loading={loading}
-        pagination={false}
-        size="small"
-        dataSource={events}
-        bordered
-        tableLayout="fixed"
-        columns={[
-          {
-            dataIndex: "type",
-            title: "Type",
-            ellipsis: true,
-            width: 100,
-            filters: [
-              {
-                text: "Normal",
-                value: DeploymentEventTypes.Normal,
+      <ConfigProvider
+        renderEmpty={() => (
+          <Empty
+            image={Empty.PRESENTED_IMAGE_SIMPLE}
+            description="No events in the past 1 hour"
+          />
+        )}
+      >
+        <Table
+          loading={loading}
+          pagination={false}
+          size="small"
+          dataSource={events}
+          bordered
+          tableLayout="fixed"
+          footer={events.length ? () => "Events in the past 1 hour" : undefined}
+          columns={[
+            {
+              dataIndex: "type",
+              title: "Type",
+              ellipsis: true,
+              width: 100,
+              filters: [
+                {
+                  text: "Normal",
+                  value: DeploymentEventTypes.Normal,
+                },
+                {
+                  text: "Warning",
+                  value: DeploymentEventTypes.Warning,
+                },
+              ],
+              render: (type) => {
+                switch (type) {
+                  case DeploymentEventTypes.Normal:
+                    return <Tag color="default">Normal</Tag>;
+                  case DeploymentEventTypes.Warning:
+                    return <Tag color="warning">Warning</Tag>;
+                  default:
+                    return <Tag color="default">{type}</Tag>;
+                }
               },
-              {
-                text: "Warning",
-                value: DeploymentEventTypes.Warning,
-              },
-            ],
-            render: (type) => {
-              switch (type) {
-                case DeploymentEventTypes.Normal:
-                  return <Tag color="default">Normal</Tag>;
-                case DeploymentEventTypes.Warning:
-                  return <Tag color="warning">Warning</Tag>;
-                default:
-                  return <Tag color="default">{type}</Tag>;
-              }
             },
-          },
-          {
-            dataIndex: "last_observed_time",
-            title: "Time",
-            ellipsis: true,
-            width: 200,
-            sorter: (a, b) =>
-              dayjs(a.last_observed_time).valueOf() -
-              dayjs(b.last_observed_time).valueOf(),
-            render: (time) => dayjs(time).format("YYYY-MM-DD HH:mm:ss"),
-          },
-          {
-            dataIndex: "reason",
-            title: "Reason",
-            ellipsis: true,
-          },
-        ]}
-      />
+            {
+              dataIndex: "last_observed_time",
+              title: "Time",
+              ellipsis: true,
+              width: 200,
+              sorter: (a, b) =>
+                dayjs(a.last_observed_time).valueOf() -
+                dayjs(b.last_observed_time).valueOf(),
+              render: (time) => dayjs(time).format("YYYY-MM-DD HH:mm:ss"),
+            },
+            {
+              dataIndex: "reason",
+              title: "Reason",
+              ellipsis: true,
+            },
+          ]}
+        />
+      </ConfigProvider>
     </Card>
   );
 };
