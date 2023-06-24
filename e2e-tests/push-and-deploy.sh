e67ad190827980373d6ea24520308b42379060f9
Cong Ding <cong@ccding.com>
2023-06-23 20:23:00 -0500

    feat(ci): using mothership to create ci workspaces (#869)

--- e2e-tests/push-and-deploy.sh
+++ e2e-tests/push-and-deploy.sh
@@ -5,6 +5,7 @@ set -x
 
 NAMESPACE=$1
 if [ -z "$NAMESPACE" ]; then
+    echo "No namespace provided"
     exit 1
 fi
 
@@ -13,20 +14,12 @@ if [ -z "$TOKEN" ]; then
     echo "No token provided: continuing without auth token"
 fi
 
-REMOTE=
-# try getting the remote address for 1 minute
-for _ in $(seq 1 60); do
-    REMOTE=$(kubectl -n "$NAMESPACE" get ingress lepton-api-server-ingress -o jsonpath='{.metadata.annotations.external-dns\.alpha\.kubernetes\.io/hostname}')
-    if [ -n "$REMOTE" ]; then
-        break
-    fi
-    sleep 1
-done
+REMOTE=$3
 if [ -z "$REMOTE" ]; then
+    echo "No remote url provided"
     exit 1
 fi
 
-REMOTE=https://$REMOTE/api/v1
 # run up to 60-minute as we add more e2e tests for example models
 if ! COLUMNS=2000 go test -timeout 3600s -v ./e2e-tests/... --remote-url "$REMOTE" --auth-token "$TOKEN"; then
     exit 1
