683c17a4282f78ce5484a20ba3b57c26394b051e
Gyuho Lee <133369823+0xgyuho@users.noreply.github.com>
2023-06-27 10:36:21 -0700

    nits(infra): add cleanup script (#982)
    Signed-off-by: Gyuho Lee <gyuho@lepton.ai>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ infra/terraform/cleanup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+targets=(
+    "eks-lepton"
+    "eks-mothership"
+    "workspace"
+)
+
+for target in "${targets[@]}"
+do
+    echo "cleaning up ${target}"
+    rm -rf "${target}"/.terraform
+    rm -rf "${target}"/.terraform.lock.hcl
+    rm -rf "${target}"/./charts
+done
