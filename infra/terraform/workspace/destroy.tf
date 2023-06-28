c35afda2617ef6e97bff02d1311008d71fed5b0b
Cong Ding <cong@ccding.com>
2023-06-28 08:43:10 -0500

    feat(infra): delete deployments before deleting the operator (#1010)

---
+++ infra/terraform/workspace/destroy.tf
@@ -0,0 +1,19 @@
+resource "null_resource" "delete_all_lepton_deployments" {
+  triggers = {
+    region       = var.region
+    cluster_name = var.cluster_name
+    namespace = var.namespace
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<-EOD
+aws eks update-kubeconfig --region ${self.triggers.region} --name ${self.triggers.cluster_name} --kubeconfig /tmp/${self.triggers.cluster_name}.kubeconfig
+kubectl --kubeconfig /tmp/${self.triggers.cluster_name}.kubeconfig -n ${self.triggers.namespace} delete leptondeployments --all
+EOD
+  }
+
+  depends_on = [
+    resource.helm_release.lepton
+   ]
+}
