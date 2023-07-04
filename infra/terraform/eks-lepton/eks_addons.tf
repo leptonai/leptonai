907dfa46083bfbbef824b03361ff9f599390e10e
Gyuho Lee <6799218+gyuho@users.noreply.github.com>
2023-07-04 12:37:23 -0700

    feat(eks-lepton): move "ebs_csi_driver_irsa" to add-on (#1170)
    Signed-off-by: Gyuho Lee <gyuho@lepton.ai>

--- infra/terraform/eks-lepton/eks_addons.tf
+++ infra/terraform/eks-lepton/eks_addons.tf
@@ -1,3 +1,16 @@
+module "ebs_csi_driver_irsa" {
+  source                = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version               = "~> 5.14"
+  role_name             = format("%s-%s", local.cluster_name, "ebs-csi-driver")
+  attach_ebs_csi_policy = true
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+
 module "eks_blueprints_kubernetes_addons" {
   source = "github.com/aws-ia/terraform-aws-eks-blueprints-addons?ref=ac7fd74d9df282ce6f8d068c4fd17ccd5638ae3a"
 
--- infra/terraform/eks-lepton/main.tf
+++ infra/terraform/eks-lepton/main.tf
@@ -238,16 +238,3 @@ resource "kubernetes_annotations" "gp2_sc_non_default" {
     "storageclass.kubernetes.io/is-default-class" = "false"
   }
 }
-
-module "ebs_csi_driver_irsa" {
-  source                = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version               = "~> 5.14"
-  role_name             = format("%s-%s", local.cluster_name, "ebs-csi-driver")
-  attach_ebs_csi_policy = true
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
-    }
-  }
-}
