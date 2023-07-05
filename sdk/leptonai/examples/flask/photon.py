bccef051121f77b419abaa9e632f3c04ebf7301a
bddppq <jbai@lepton.ai>
2023-07-04 23:19:03 -0700

    fix(sdk): update flask app example (#1185)

--- sdk/leptonai/examples/flask/photon.py
+++ sdk/leptonai/examples/flask/photon.py
@@ -1,13 +1,12 @@
-import os
-import sys
-
 from leptonai.photon import Photon
 
 
 class FlaskPhoton(Photon):
+    extra_files = {"flask_app-0.1-py3-none-any.whl": "flask_app-0.1-py3-none-any.whl"}
+    requirement_dependency = ["flask_app-0.1-py3-none-any.whl"]
+
     @Photon.handler("", mount=True)
     def flask_app(self):
-        sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-        from app import app
+        from flask_app import app
 
         return app
