971f72d3cdf50f451aa009198d1263c91a0c829e
Fanmin Shi <fanmin.shi@gmail.com>
2023-07-06 13:20:33 -0700

    fix(vectordb): allow vectordb to be exported (#1210)

---
+++ vectordb/vectordb/api/__init__.py
@@ -0,0 +1,12 @@
+from .collection import Collection
+from .types import Vector, Result, SearchResponse, GetResponse, Embedding, Metadata
+
+__all__ = [
+    "Collection",
+    "Vector",
+    "Result",
+    "SearchResponse",
+    "GetResponse",
+    "Embedding",
+    "Metadata",
+]
---
+++ vectordb/vectordb/client/tests/test_client.py
@@ -0,0 +1,6 @@
+from vectordb.client.client import Client
+import unittest
+
+
+class TestClient(unittest.TestCase):
+    pass
