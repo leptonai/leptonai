e6088cb54de15fdfe309f24caf213792950b7649
Xiang Li <xiangli.cs@gmail.com>
2023-07-06 15:42:02 -0700

    fix(infra): disable network policy (#1246)

--- infra/terraform/eks-lepton/network_policy.tf
+++ infra/terraform/eks-lepton/network_policy.tf
@@ -1,94 +1,96 @@
-locals {
-  non_lepton_managed_namespace_selector = "has(projectcalico.org/name) && projectcalico.org/name not in {\"default\", \"kube-system\", \"cert-manager\", \"calico-system\", \"calico-apiserver\", \"external-dns\", \"gpu-operator\", \"prometheus\", \"grafana\", \"${local.cluster_name}sys\"}"
-}
+# disable for now until we fix node to pod access (for liveness/readiness probe)
+#
+# locals {
+#   non_lepton_managed_namespace_selector = "has(projectcalico.org/name) && projectcalico.org/name not in {\"default\", \"kube-system\", \"cert-manager\", \"calico-system\", \"calico-apiserver\", \"external-dns\", \"gpu-operator\", \"prometheus\", \"grafana\", \"lepton-system\"}"
+# }
 
-resource "kubernetes_manifest" "allow_external" {
-  manifest = {
-    apiVersion = "projectcalico.org/v3"
-    kind       = "GlobalNetworkPolicy"
+# resource "kubernetes_manifest" "allow_external" {
+#   manifest = {
+#     apiVersion = "projectcalico.org/v3"
+#     kind       = "GlobalNetworkPolicy"
 
-    metadata = {
-      name = "allow-external-policy"
-    }
+#     metadata = {
+#       name = "allow-external-policy"
+#     }
 
-    spec = {
-      namespaceSelector = local.non_lepton_managed_namespace_selector
+#     spec = {
+#       namespaceSelector = local.non_lepton_managed_namespace_selector
 
-      types = ["Egress"]
+#       types = ["Egress"]
 
-      egress = [
-        {
-          action = "Allow"
+#       egress = [
+#         {
+#           action = "Allow"
 
-          destination = {
-            nets = ["${module.vpc.vpc_cidr_block}"]
-          }
-        }
-      ]
-    }
-  }
+#           destination = {
+#             nets = ["${module.vpc.vpc_cidr_block}"]
+#           }
+#         }
+#       ]
+#     }
+#   }
 
-  depends_on = [helm_release.calico]
-}
+#   depends_on = [helm_release.calico]
+# }
 
-resource "kubernetes_manifest" "allow_system_as_dest_policy" {
-  manifest = {
-    apiVersion = "projectcalico.org/v3"
-    kind       = "GlobalNetworkPolicy"
+# resource "kubernetes_manifest" "allow_system_as_dest_policy" {
+#   manifest = {
+#     apiVersion = "projectcalico.org/v3"
+#     kind       = "GlobalNetworkPolicy"
 
-    metadata = {
-      name = "allow-system-as-dest-policy"
-    }
+#     metadata = {
+#       name = "allow-system-as-dest-policy"
+#     }
 
-    spec = {
-      namespaceSelector = local.non_lepton_managed_namespace_selector
+#     spec = {
+#       namespaceSelector = local.non_lepton_managed_namespace_selector
 
-      types = ["Egress"]
+#       types = ["Egress"]
 
-      egress = [
-        {
-          action = "Allow"
+#       egress = [
+#         {
+#           action = "Allow"
 
-          destination = {
-            namespaceSelector = "has(projectcalico.org/name) && projectcalico.org/name in {\"default\", \"kube-system\", \"external-dns\", \"lepton-system\", \"prometheus\"}"
-          }
-        }
-      ]
-    }
-  }
+#           destination = {
+#             namespaceSelector = "has(projectcalico.org/name) && projectcalico.org/name in {\"default\", \"kube-system\", \"external-dns\", \"lepton-system\", \"prometheus\"}"
+#           }
+#         }
+#       ]
+#     }
+#   }
 
-  depends_on = [helm_release.calico]
-}
+#   depends_on = [helm_release.calico]
+# }
 
-resource "kubernetes_manifest" "allow_kube_apiserver_policy" {
-  manifest = {
-    apiVersion = "projectcalico.org/v3"
-    kind       = "GlobalNetworkPolicy"
+# resource "kubernetes_manifest" "allow_kube_apiserver_policy" {
+#   manifest = {
+#     apiVersion = "projectcalico.org/v3"
+#     kind       = "GlobalNetworkPolicy"
 
-    metadata = {
-      name = "allow-kube-apiserver-policy"
-    }
+#     metadata = {
+#       name = "allow-kube-apiserver-policy"
+#     }
 
-    spec = {
-      namespaceSelector = local.non_lepton_managed_namespace_selector
+#     spec = {
+#       namespaceSelector = local.non_lepton_managed_namespace_selector
 
-      types    = ["Egress"]
-      selector = "app in {\"lepton-deployment-operator\", \"lepton-api-server\"}"
+#       types    = ["Egress"]
+#       selector = "app in {\"lepton-deployment-operator\", \"lepton-api-server\"}"
 
-      egress = [
-        {
-          action = "Allow"
+#       egress = [
+#         {
+#           action = "Allow"
 
-          destination = {
-            services = {
-              name      = "kubernetes"
-              namespace = "default"
-            }
-          }
-        }
-      ]
-    }
-  }
+#           destination = {
+#             services = {
+#               name      = "kubernetes"
+#               namespace = "default"
+#             }
+#           }
+#         }
+#       ]
+#     }
+#   }
 
-  depends_on = [helm_release.calico]
-}
+#   depends_on = [helm_release.calico]
+# }
