a4e0cc15d9d1d4758e7e6b84fd8b2f3d374883b6
Yangqing Jia <jia@lepton.ai>
2023-07-11 15:03:26 -0700

    chore(web): remove cpu and memory info from resource type display (#1418)
    Per offline discussion and closes #1416

--- web/src/services/hardware.service.ts
+++ web/src/services/hardware.service.ts
@@ -7,7 +7,7 @@ export class HardwareService {
     "cpu.small": {
       Description:
         "General purpose small shape with 1 CPUs, 4GB of RAM and 16GB of ephemeral storage",
-      DisplayName: "cpu.small - [1c4g]",
+      DisplayName: "cpu.small",
       Resource: {
         CPU: 1,
         Memory: 4096,
@@ -17,7 +17,7 @@ export class HardwareService {
     "cpu.medium": {
       Description:
         "General purpose medium shape with 2 CPUs, 8GB of RAM and 32GB of ephemeral storage",
-      DisplayName: "cpu.medium - [2c8g]",
+      DisplayName: "cpu.medium",
       Resource: {
         CPU: 2,
         Memory: 8192,
@@ -27,7 +27,7 @@ export class HardwareService {
     "cpu.large": {
       Description:
         "General purpose large shape with 4 CPUs, 16GB of RAM and 64GB of ephemeral storage",
-      DisplayName: "cpu.large - [4c16g]",
+      DisplayName: "cpu.large",
       Resource: {
         CPU: 4,
         Memory: 16384,
@@ -37,7 +37,7 @@ export class HardwareService {
     "gpu.t4": {
       Description:
         "Accelerated computing shape with 1 16GB T4 GPU, 4 CPUs, 16GB of RAM and 100GB of ephemeral storage",
-      DisplayName: "gpu.t4 - [4c16g]",
+      DisplayName: "gpu.t4",
       Resource: {
         CPU: 4,
         Memory: 16384,
@@ -49,12 +49,12 @@ export class HardwareService {
     "gpu.a10": {
       Description:
         "Accelerated computing shape with 1 24GB A10 GPU, 8 CPUs, 32GB of RAM and 400GB of ephemeral storage",
-      DisplayName: "gpu.a10 - [8c32g]",
+      DisplayName: "gpu.a10",
       Resource: {
         CPU: 8,
         Memory: 32768,
         EphemeralStorageInGB: 400,
-        AcceleratorType: "NVIDIA-A10G",
+        AcceleratorType: "NVIDIA-A10",
         AcceleratorNum: 1,
       },
     },
