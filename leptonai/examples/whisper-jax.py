318a1a21821a48fec9d37b4effb10abb5d07ea19
bddppq <jbai@lepton.ai>
2023-07-15 09:50:59 -0700

    example(sdk): Extend whisper example to add slack bot functionality (https://github.com/leptonai/lepton/issues/1549)

--- leptonai/examples/whisper-jax.py
+++ leptonai/examples/whisper-jax.py
@@ -11,10 +11,15 @@ Lepton's default image.
 
 """
 
+from datetime import datetime, timedelta
 import os
-from typing import Optional
+import tempfile
+from typing import Optional, Dict, Any
 
-from leptonai.photon import Photon
+from loguru import logger
+import requests
+
+from leptonai.photon import Photon, HTTPException
 
 
 class Whisper(Photon):
@@ -24,6 +29,7 @@ class Whisper(Photon):
         "-f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html",
         "jax==0.4.13",
         "jaxlib==0.4.13+cuda11.cudnn86",
+        "slack_sdk",
     ]
 
     system_dependency = [
@@ -40,6 +46,17 @@ class Whisper(Photon):
             model_id, dtype=jnp.float16, batch_size=batch_size
         )
 
+        self._init_slack_bot()
+
+    def _init_slack_bot(self):
+        from slack_sdk import WebClient as SlackClient
+
+        self._verification_token = os.environ.get("SLACK_VERIFICATION_TOKEN", None)
+        self._slack_bot_token = os.environ.get("SLACK_BOT_TOKEN", None)
+        if self._slack_bot_token:
+            self._slack_bot_client = SlackClient(token=self._slack_bot_token)
+        self._processed_slack_tasks = {}
+
     @Photon.handler(
         "run",
         example={
@@ -49,4 +66,74 @@ class Whisper(Photon):
         },
     )
     def run(self, inputs: str, task: Optional[str] = None):
-        return self.pipeline(inputs, task=task)
+        return self.pipeline(inputs, task=task)["text"]
+
+    def _slack_process_task(self, channel: str, thread_ts: str, url: str):
+        last_processed_time = self._processed_slack_tasks.get((channel, url))
+        if last_processed_time and datetime.now() - last_processed_time < timedelta(
+            seconds=20
+        ):
+            logger.info(
+                f"Skip processing slack task: ({channel}, {url}) since it was processed"
+                f" recently: {last_processed_time}"
+            )
+            return
+
+        logger.info(f"Processing audio file: {url}")
+        with tempfile.NamedTemporaryFile("wb", suffix="." + url.split(".")[-1]) as f:
+            logger.info(f"Start downloading audio file to: {f.name}")
+            res = requests.get(
+                url,
+                allow_redirects=True,
+                headers={"Authorization": f"Bearer {self._slack_bot_token}"},
+            )
+            res.raise_for_status()
+            logger.info(f"Downloaded audio file (total bytes: {len(res.content)})")
+            f.write(res.content)
+            f.flush()
+            f.seek(0)
+            logger.info(f"Saved audio file to: {f.name}")
+            logger.info(f"Running inference on audio file: {f.name}")
+            text = self.run(f.name)
+            logger.info(f"Finished inference on audio file: {f.name}")
+        self._slack_bot_client.chat_postMessage(
+            channel=channel,
+            thread_ts=thread_ts,
+            text=text,
+        )
+        self._processed_slack_tasks[(channel, url)] = datetime.now()
+
+    @Photon.handler
+    def slack(
+        self,
+        token: str,
+        type: str,
+        event: Optional[Dict[str, Any]] = None,
+        challenge: Optional[str] = None,
+        **extra,
+    ) -> str:
+        if not self._verification_token or not self._slack_bot_token:
+            raise HTTPException(401, "Slack bot not configured")
+
+        if token != self._verification_token:
+            raise HTTPException(401, "Invalid token")
+
+        if type == "url_verification":
+            return challenge
+
+        event_type = event["type"]
+        logger.info(f"Received slack event: {event_type}")
+
+        if event_type == "file_shared":
+            channel = event["channel_id"]
+            thread_ts = event.get("thread_ts")
+            file_id = event["file_id"]
+            file_info = self._slack_bot_client.files_info(file=file_id)
+            if not file_info["ok"]:
+                raise HTTPException(500, "Failed to get file info")
+            self._slack_process_task(
+                channel, thread_ts, file_info["file"]["url_private"]
+            )
+            return "ok"
+
+        logger.info(f"Ignored slack event type: {event_type}")
