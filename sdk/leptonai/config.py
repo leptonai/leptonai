4a1a23249d0218047dee8d7adc8ad0e47fae6567
bddppq <jbai@lepton.ai>
2023-07-14 17:07:42 -0700

    feat(sdk): Support GET method in Photon.handler (#1535)

--- sdk/leptonai/config.py
+++ sdk/leptonai/config.py
@@ -13,7 +13,7 @@ if not CACHE_DIR.exists():
 
 DB_PATH = CACHE_DIR / "lepton.db"
 
-BASE_IMAGE_VERSION = "0.1.8"
+BASE_IMAGE_VERSION = "0.1.9"
 BASE_IMAGE_REPO = "605454121064.dkr.ecr.us-east-1.amazonaws.com/lepton"
 BASE_IMAGE = f"{BASE_IMAGE_REPO}:photon-py{sys.version_info.major}.{sys.version_info.minor}-runner-{BASE_IMAGE_VERSION}"
 BASE_IMAGE_ARGS = ["--shm-size=1g"]
