108190d8fd0acf0bb88a41d7fcaa9ab32049eb8a
bddppq <jbai@lepton.ai>
2023-07-15 21:45:33 -0700

    feat(sdk): setup sdk versioning (https://github.com/leptonai/lepton/issues/1565)
    https://github.com/leptonai/lepton/issues/1498
    
    - at package build time, generates the version number (mechanism
    detailed below) into a file leptonai/_version.py, and at runtime we
    expose this version number as `leptonai.__version__` python variable,
    all other python code should read this variable as the source of truth
    version number
    - version number is associated with git tags, current workdir git
    commit, and whether current workdir has git modificaitions ("dirty"):
    * if current commit has a tag exactly matches semver format (e.g.
    0.1.0), git workdir is clean, then the version number is the semver
    (i.e. `0.1.0`). This is the case when we release a package
       * if current commit doesn't have a tag associated
    * it traces back the git history to last one that has tag, and use its
    tag + the number of commits after it as the version number + current git
    sha (e.g. `0.1.0.post1.dev10+ga8850795`) note the sha part is prepended
    with a "g". This is the case when we send packages internally, or
    sometimes adhoc building a package to (very) close partner
    * if in addition current directory is dirty, it will append "+d{date}"
    to the version number, e.g. (`0.1.0.post1.dev10+ga8850795.d20230715`).
    This should only for development, don't send such packages to others.
    
    If we want to associate the version number with our sprint, create a git
    tag at the end of every sprint, e.g
    (b7ba952e65cf6a376b2efa5bc8f61b900e944a80 is the last commit on 07/07)
    ``` shell
    $ git tag 0.5.0 b7ba952e
    $ git push origin 0.5.0
    
    $ lep -v 
    lep, version 0.5.0
    ```

--- leptonai/__init__.py
+++ leptonai/__init__.py
@@ -1,2 +1,3 @@
 from . import photon  # noqa: F401
 from .client import Client  # noqa: F401
+from ._version import __version__  # noqa: F401
--- leptonai/tests/test_scaffold.py
+++ leptonai/tests/test_scaffold.py
@@ -7,6 +7,11 @@ class TestImport(unittest.TestCase):
 
         self.assertTrue(hasattr(leptonai, "photon"))
 
+    def test_version(self):
+        from leptonai import __version__
+
+        self.assertIsNotNone(__version__)
+
 
 if __name__ == "__main__":
     unittest.main()
