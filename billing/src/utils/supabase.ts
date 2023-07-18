75005af1d9282df9c3eed8210b8214457c9246ec
Yadong Xie <vthinkxie@gmail.com>
2023-07-18 18:01:40 +0800

    fix(billing): fix no storage option exists warning (#1622)

--- billing/src/utils/supabase.ts
+++ billing/src/utils/supabase.ts
@@ -3,4 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 export const supabase = createClient(
   process.env.SUPABASE_URL!,
   process.env.SUPABASE_SECRET_KEY!,
+  {
+    auth: {
+      persistSession: false,
+    },
+  },
 );
