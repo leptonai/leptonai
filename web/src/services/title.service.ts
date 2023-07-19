90c869a34a982a178c98f179ec43cd311c4be979
Hsuan Lee <HsuanGM@gmail.com>
2023-07-19 11:33:58 +0800

    feat(web): add open graph meta (#1644)
    close #1587

--- web/src/services/title.service.ts
+++ web/src/services/title.service.ts
@@ -5,7 +5,15 @@ import { ReplaySubject } from "rxjs";
 export class TitleService {
   title$ = new ReplaySubject<string>(1);
   setTitle(title: string) {
+    const ogTitle = document.querySelector("meta[property='og:title']");
+    const twitterTitle = document.querySelector("meta[name='twitter:title']");
     document.title = `${title} | Lepton AI`;
+    if (ogTitle) {
+      ogTitle.setAttribute("content", `${title} | Lepton AI`);
+    }
+    if (twitterTitle) {
+      twitterTitle.setAttribute("content", `${title} | Lepton AI`);
+    }
     this.title$.next(title);
   }
 }
