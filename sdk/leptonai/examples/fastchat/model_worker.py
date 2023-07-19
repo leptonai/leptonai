362b1f39d13f7bd495d59653b16124e6976e741d
bddppq <jbai@lepton.ai>
2023-07-19 16:06:18 -0700

    feat(tuna): move tuna photons to tuna directory and update fastchat examples (#1663)

--- sdk/leptonai/examples/fastchat/model_worker.py
+++ sdk/leptonai/examples/fastchat/model_worker.py
@@ -1,14 +1,14 @@
-from collections import namedtuple
 import os
 
-from leptonai.config import BASE_IMAGE_REPO, BASE_IMAGE_VERSION
 from leptonai.photon import Photon
 
 import fastchat.serve.model_worker
 
 
 class Server(Photon):
-    image: f"{BASE_IMAGE_REPO}:tuna-runner-{BASE_IMAGE_VERSION}"
+    requirement_dependency = [
+        "git+https://github.com/lm-sys/FastChat.git@974537e",
+    ]
 
     def init(self):
         worker = fastchat.serve.model_worker.ModelWorker(
@@ -18,7 +18,7 @@ class Server(Photon):
             no_register=False,
             model_path=os.environ.get("MODEL_PATH", "./model"),
             model_names=[
-                "tuna",
+                os.environ.get("MODEL_NAME", "tuna"),
                 "gpt-3.5-turbo",
                 "text-davinci-003",
                 "text-embedding-ada-002",
@@ -29,10 +29,8 @@ class Server(Photon):
             load_8bit=False,
             cpu_offloading=False,
             gptq_config=None,
-        )
-        FakeArgs = namedtuple("Args", ["limit_model_concurrency", "stream_interval"])
-        fastchat.serve.model_worker.args = FakeArgs(
-            limit_model_concurrency=5, stream_interval=2
+            limit_worker_concurrency=5,
+            stream_interval=2,
         )
         fastchat.serve.model_worker.worker = worker
 
