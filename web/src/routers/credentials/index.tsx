801ab6f3adcfe6cb6120e276a4b2de0d05e8ac48
Yadong Xie <vthinkxie@gmail.com>
2023-07-20 20:01:21 +0800

    chore(web): better credential ux (#1683)

--- web/src/routers/credentials/index.tsx
+++ web/src/routers/credentials/index.tsx
@@ -1,14 +1,10 @@
+import { css } from "@emotion/react";
 import { useInject } from "@lepton-libs/di";
 import { AuthService } from "@lepton-dashboard/services/auth.service";
 import { useStateFromObservable } from "@lepton-libs/hooks/use-state-from-observable";
-import styled from "@emotion/styled";
 import { Table, Typography } from "antd";
-import { CarbonIcon } from "@lepton-dashboard/components/icons";
+import { CarbonIcon, LeptonIcon } from "@lepton-dashboard/components/icons";
 import { CopyFile } from "@carbon/icons-react";
-
-const Container = styled.div`
-  padding: 32px;
-`;
 
 export const Credentials = () => {
   const authService = useInject(AuthService);
@@ -17,34 +13,63 @@ export const Credentials = () => {
     []
   );
   return (
-    <Container>
-      <Typography.Title level={2}>Credentials</Typography.Title>
-      <Table
-        pagination={false}
-        size="small"
-        bordered
-        dataSource={workspaces}
-        columns={[
-          {
-            title: "Workspace Name",
-            dataIndex: "displayName",
-          },
-          {
-            title: "Credentials",
-            dataIndex: "token",
-            render: (v, record) => (
-              <Typography.Text
-                copyable={{
-                  icon: <CarbonIcon icon={<CopyFile />} />,
-                  text: `${record.id}:${v}`,
-                }}
-              >
-                ••••••••••••
-              </Typography.Text>
-            ),
-          },
-        ]}
-      />
-    </Container>
+    <div
+      css={css`
+        display: flex;
+        justify-content: center;
+        padding-top: 12%;
+      `}
+    >
+      <div
+        css={css`
+          padding: 32px;
+        `}
+      >
+        <Typography.Title
+          level={2}
+          css={css`
+            margin-top: 0;
+          `}
+        >
+          <LeptonIcon
+            css={css`
+              margin-right: 8px;
+            `}
+          />
+          Credentials
+        </Typography.Title>
+        <Typography.Paragraph type="secondary">
+          You have the following workspaces available. Copy the credential
+          string of the workspace you want to log in, and paste it in the
+          commandline.
+        </Typography.Paragraph>
+        <Table
+          pagination={false}
+          size="small"
+          bordered
+          dataSource={workspaces}
+          rowKey="id"
+          columns={[
+            {
+              title: "NAME",
+              dataIndex: "displayName",
+            },
+            {
+              title: "CREDENTIAL",
+              dataIndex: "token",
+              render: (v, record) => (
+                <Typography.Text
+                  copyable={{
+                    icon: <CarbonIcon icon={<CopyFile />} />,
+                  }}
+                >
+                  {record.id}:{v}
+                </Typography.Text>
+              ),
+            },
+          ]}
+        />
+      </div>
+    </div>
   );
 };
