1fefa58d51d3c6b52eae77c0cb27432049b5c786
bddppq <jbai@lepton.ai>
2023-07-25 13:15:30 -0700

    fix(tuna): fix bug of setting tuna image (#1863)

--- tuna/lepton/tuna-inference.py
+++ tuna/lepton/tuna-inference.py
@@ -8,7 +8,7 @@ from leptonai.photon import Photon
 
 
 class Server(Photon):
-    image: f"{BASE_IMAGE_REPO}:tuna-23.02"
+    image: str = f"{BASE_IMAGE_REPO}:tuna-23.02"
 
     @staticmethod
     def _patch_get_model_list(controller_url):
