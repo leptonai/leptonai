e529efae37b94480cda77c24a7695e7a6eabf045
Cong Ding <cong@ccding.com>
2023-07-26 17:21:18 -0500

    fix(e2e-tests): make sure there are data points in metrics (#1891)

--- e2e-tests/monitoring_test.go
+++ e2e-tests/monitoring_test.go
@@ -2,6 +2,7 @@ package e2etests
 
 import (
 	"encoding/json"
+	"strings"
 	"testing"
 )
 
@@ -32,20 +33,28 @@ func TestReplicaMonitoring(t *testing.T) {
 	for _, name := range replicaMonitoringInterfaces {
 		replicas, err := lepton.Replica().List(mainTestDeploymentName)
 		if err != nil {
-			t.Fatalf("failed to list replicas: %s", err)
+			t.Errorf("failed to list replicas: %s", err)
 		}
 		if len(replicas) < 1 {
-			t.Fatalf("no replicas found for deployment %s", mainTestDeploymentName)
+			t.Errorf("no replicas found for deployment %s", mainTestDeploymentName)
 		}
 		replicaID := replicas[0].ID
 		raw, err := lepton.Monitoring().GetReplicaRaw(name, mainTestDeploymentName, replicaID)
 		if err != nil {
-			t.Fatalf("failed to get replica raw for name %s and replica %s: %s", name, replicaID, err)
+			t.Errorf("failed to get replica raw for name %s and replica %s: %s", name, replicaID, err)
 		}
 		s := []interface{}{}
 		err = json.Unmarshal([]byte(raw), &s)
 		if err != nil {
-			t.Fatalf("failed to unmarshal output for name %s and replica %s: %s, with output: %s", name, replicaID, err, raw)
+			t.Errorf("failed to unmarshal output for name %s and replica %s: %s, with output: %s", name, replicaID, err, raw)
+		}
+		if len(s) == 0 {
+			// skip GPU monitoring because no GPU is available in CI clusters
+			if !strings.HasPrefix(name, "GPU") &&
+				// skip FastAPI monitoring because no request was sent so no data is available
+				!strings.HasPrefix(name, "FastAPI") {
+				t.Errorf("failed to get any data for name %s and replica %s", name, replicaID)
+			}
 		}
 	}
 }
@@ -54,12 +63,20 @@ func TestDeploymentMonitoring(t *testing.T) {
 	for _, name := range deploymentMonitoringInterfaces {
 		raw, err := lepton.Monitoring().GetDeploymentRaw(name, mainTestDeploymentName)
 		if err != nil {
-			t.Fatalf("failed to get deployment raw for name %s: %s", name, err)
+			t.Errorf("failed to get deployment raw for name %s: %s", name, err)
 		}
 		s := []interface{}{}
 		err = json.Unmarshal([]byte(raw), &s)
 		if err != nil {
-			t.Fatalf("failed to unmarshal output for name %s: %s, with output %s", name, err, raw)
+			t.Errorf("failed to unmarshal output for name %s: %s, with output %s", name, err, raw)
+		}
+		if len(s) == 0 {
+			// skip GPU monitoring because no GPU is available in CI clusters
+			if !strings.HasPrefix(name, "GPU") &&
+				// skip FastAPI monitoring because no request was sent so no data is available
+				!strings.HasPrefix(name, "FastAPI") {
+				t.Errorf("failed to get any data for name %s", name)
+			}
 		}
 	}
 }
