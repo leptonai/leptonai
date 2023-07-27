3badb8b6ae3164f99f0c5ddd166cd78c94a8dacb
bddppq <jbai@lepton.ai>
2023-07-26 23:18:51 -0700

    feat(sdk): stable diffusion webui update (#1923)
    * update to newest release
    [v1.5.0](https://github.com/AUTOMATIC1111/stable-diffusion-webui/releases/tag/v1.5.0)
    * enable api

--- sdk/leptonai/examples/sd_webui.py
+++ sdk/leptonai/examples/sd_webui.py
@@ -7,26 +7,13 @@ from leptonai.photon import Photon
 
 
 class WebUI(Photon):
-    vcs_url: str = "https://github.com/AUTOMATIC1111/stable-diffusion-webui.git@v1.3.0"
+    vcs_url: str = "https://github.com/AUTOMATIC1111/stable-diffusion-webui.git@v1.5.0"
     requirement_dependency = ["loguru", "gradio==3.32.0", "pydantic<2.0.0"]
     system_dependency = ["libgl1"]
 
     @Photon.handler("", mount=True)
     def ui(self, app):
-        # patch gradio version, 3.31.0 is known to have a bug causes mounting
-        # not correctly working:
-        # https://github.com/gradio-app/gradio/issues/4291
-        pip_file = os.path.join(os.getcwd(), "requirements_versions.txt")
-        deps = []
-        with open(pip_file, "r") as f:
-            for line in f:
-                if line.startswith("gradio"):
-                    line = "gradio==3.32.0"
-                deps.append(line.strip())
-        with open(pip_file, "w") as f:
-            f.write(os.linesep.join(deps))
-
-        # currently directory should be a checkout of stable-diffusion-webui
+        # current directory should be a checkout of stable-diffusion-webui
         # git repo, adding it to sys.path so that we can `import modules`
         sys.path.append(os.getcwd())
 
@@ -60,6 +47,7 @@ class WebUI(Photon):
 
         progress.setup_progress_api(app)
         ui.setup_ui_api(app)
+        webui.create_api(app)
 
         from modules import ui_extra_networks
 
