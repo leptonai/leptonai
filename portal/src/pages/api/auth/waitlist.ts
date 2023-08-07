6689a86519220b8eff4dbe4618dfa93caf66a94b
Hsuan Lee <HsuanGM@gmail.com>
2023-08-07 18:15:26 +0800

    fix(portal): full the parameters of the join_waitlist (#2325)

--- portal/src/pages/api/auth/waitlist.ts
+++ portal/src/pages/api/auth/waitlist.ts
@@ -10,7 +10,14 @@ const waitlist: NextApiWithSupabaseHandler = async (req, res, supabase) => {
   }
   const body = req.body;
 
-  const { data, error } = await supabase.rpc("join_waitlist", body);
+  const { data, error } = await supabase.rpc("join_waitlist", {
+    name: body.name,
+    company: body.company,
+    role: body.role,
+    company_size: body.company_size || "",
+    industry: body.industry || "",
+    work_email: body.work_email || "",
+  });
 
   if (error) {
     return res.status(500).json({ error: error.message });
