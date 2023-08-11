38bceb5d356711be563f679519f9c31e13ef8f0c
Xiang Li <xiangli.cs@gmail.com>
2023-08-11 15:55:18 -0700

    feat(go-pkg): do not log context canceled as an error (#2486)
    @ccding

--- go-pkg/util/log.go
+++ go-pkg/util/log.go
@@ -1,12 +1,40 @@
 package util
 
-import "go.uber.org/zap"
+import (
+	"context"
+	"strings"
+
+	"go.uber.org/zap"
+)
 
 var (
-	Logger *zap.SugaredLogger
+	Logger *leptonLogger
 )
 
 func init() {
 	l, _ := zap.NewProduction()
-	Logger = l.Sugar()
+	Logger = &leptonLogger{
+		l.Sugar(),
+	}
+}
+
+type leptonLogger struct {
+	*zap.SugaredLogger
+}
+
+// Override the default logger's Errorw func to down level context canceled error
+func (l *leptonLogger) Errorw(msg string, keysAndValues ...interface{}) {
+	for i := 0; i < len(keysAndValues); i += 2 {
+		if keysAndValues[i] != "error" {
+			continue
+		}
+		if err, ok := keysAndValues[i+1].(error); ok {
+			if strings.Contains(err.Error(), context.Canceled.Error()) {
+				l.SugaredLogger.Warnw(msg, keysAndValues...)
+				return
+			}
+		}
+	}
+
+	l.SugaredLogger.Errorw(msg, keysAndValues...)
 }
