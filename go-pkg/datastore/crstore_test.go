d03491803b86718834447b3a6c3482b5e8908a1a
Xiang Li <xiangli.cs@gmail.com>
2023-08-16 21:05:46 -0700

    test(datastore): add UTs (#2609)
    Close https://github.com/leptonai/lepton/issues/2298

--- go-pkg/datastore/crstore_test.go
+++ go-pkg/datastore/crstore_test.go
@@ -5,9 +5,9 @@ import (
 	"testing"
 	"time"
 
+	leptonaiv1alpha1 "github.com/leptonai/lepton/deployment-operator/api/v1alpha1"
 	"github.com/leptonai/lepton/go-pkg/k8s"
 	"github.com/leptonai/lepton/go-pkg/util"
-	leptonaiv1alpha1 "github.com/leptonai/lepton/deployment-operator/api/v1alpha1"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -50,35 +50,79 @@ func TestCRStore(t *testing.T) {
 			t.Fatalf("Failed to delete namespace %s: %v", namespace, err)
 		}
 	}()
-	// Get
-	lc, err := s.Get(ctx, crName)
-	if err != nil {
-		t.Fatalf("Failed to get: %v", err)
-	}
-	if lc.GetName() != crName {
-		t.Fatalf("Failed to get: %v", err)
-	}
-	// List
-	lcs, err := s.List(ctx)
-	if err != nil {
-		t.Fatalf("Failed to list: %v", err)
-	}
-	found := false
-	for _, lc := range lcs {
-		if lc.GetName() == crName {
-			found = true
+
+	t.Run("TestCRStoreCreateConflict", func(t *testing.T) {
+		if err := s.Create(ctx, crName, example); err == nil {
+			t.Fatalf("Expect to fail with conflict, but not")
 		}
-	}
-	if !found {
-		t.Fatalf("Expect to find %s in list, but not found", crName)
-	}
+	})
 
-	err = s.Backup(ctx)
-	if err != nil {
-		t.Log("Failed to backup:", err)
-	}
+	t.Run("TestCRStoreUpdate", func(t *testing.T) {
+		example.Spec.Image = "test-image"
+		if err := s.Update(ctx, crName, example); err != nil {
+			t.Fatalf("Failed to update: %v", err)
+		}
+		lc, err := s.Get(ctx, crName)
+		if err != nil {
+			t.Fatalf("Failed to get: %v", err)
+		}
+		if lc.Spec.Image != "test-image" {
+			t.Fatalf("Failed to update: %v", err)
+		}
+	})
 
-	if err := s.Delete(ctx, crName); err != nil {
-		t.Fatalf("Failed to delete: %v", err)
-	}
+	t.Run("TestCRStoreUpdateNonExist", func(t *testing.T) {
+		if err := s.Update(ctx, "non-exist", example); err == nil {
+			t.Fatalf("Expect to fail with not found, but not")
+		}
+	})
+
+	t.Run("TestCRStoreGet", func(t *testing.T) {
+		lc, err := s.Get(ctx, crName)
+		if err != nil {
+			t.Fatalf("Failed to get: %v", err)
+		}
+		if lc.GetName() != crName {
+			t.Fatalf("Failed to get: %v", err)
+		}
+	})
+
+	t.Run("TestCRStoreList", func(t *testing.T) {
+		lcs, err := s.List(ctx)
+		if err != nil {
+			t.Fatalf("Failed to list: %v", err)
+		}
+		found := false
+		for _, lc := range lcs {
+			if lc.GetName() == crName {
+				found = true
+			}
+		}
+		if !found {
+			t.Fatalf("Expect to find %s in list, but not found", crName)
+		}
+	})
+
+	t.Run("TestCRStoreDeleteNonExist", func(t *testing.T) {
+		if err := s.Delete(ctx, "non-exist"); err == nil {
+			t.Fatalf("Expect to fail with not found, but not")
+		}
+	})
+
+	t.Run("TestCRStoreBackup", func(t *testing.T) {
+		err := s.Backup(ctx)
+		if err != nil {
+			t.Log("Failed to backup:", err)
+		}
+	})
+
+	t.Run("TestCRStoreDelete", func(t *testing.T) {
+		if err := s.Delete(ctx, crName); err != nil {
+			t.Fatalf("Failed to delete: %v", err)
+		}
+		_, err := s.Get(ctx, crName)
+		if err == nil {
+			t.Fatalf("Expect to fail with not found, but not")
+		}
+	})
 }
