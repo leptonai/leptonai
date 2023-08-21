e8f7c0bc86152bf0d5b1728fc88cbe5e3b020646
Yuze Ma <yuze.bob.ma@gmail.com>
2023-08-21 10:21:33 -0700

    feat(butler) : added get all ws stat func (#2719)

--- butler/backend/workspace.py
+++ butler/backend/workspace.py
@@ -76,3 +76,43 @@ def get_workspace_users(workspace_name: str):
     df = pd.read_sql(query, db_connection)
     db_connection.close()
     return df
+
+
+def get_all_ws_stats():
+    ws_total = get_workspaces()
+    workspace_ids = [w['spec']['name'] for w in ws_total]
+    out = []
+    for workspace_id in workspace_ids:
+        print(workspace_id)
+        out.append(get_ws_stat(workspace_id))
+    return pd.DataFrame(out, columns=['workspace_id', 'ws_display_name', 'num_of_photons', 'num_of_deployments', 'cpu', 'memory', 'gpu'])
+
+
+def get_ws_stat(workspace_id):
+    prefix = 'app' if ENV == 'PROD' else 'cloud'
+    base_url = "https://{}.{}.lepton.ai/api/v1/".format(workspace_id, prefix)
+    ws_info = get_workspace_info_from_mothership(workspace_id)
+    try:
+        ws_api_token = ws_info['spec']['api_token']
+    except:
+        ws_api_token = ''
+    ws_display_name = ws_info['spec']['description']
+    headers = {
+        "Authorization": "Bearer {}".format(ws_api_token),
+        "Content-Type": "application/json"
+    }   
+    # get resouce usage
+    url = base_url + "workspace"
+    res = requests.get(url, headers=headers).json()
+    cpu = round(res['resource_quota']['used']['cpu'], 2)
+    memory = round(res['resource_quota']['used']['memory'] / 1024, 2)
+    gpu = res['resource_quota']['used']['accelerator_num']
+    # get num of photons
+    url = base_url + "photons"   
+    num_of_photons = len(requests.get(url, headers=headers).json())
+    # get num of deployments
+    url = base_url + "deployments"
+    num_of_deployments = len(requests.get(url, headers=headers).json())
+    
+    info = [workspace_id, ws_display_name, num_of_photons, num_of_deployments, cpu, memory, gpu]
+    return info
