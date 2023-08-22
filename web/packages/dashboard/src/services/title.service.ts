d6b6220cebc41a5136d59d77c4f19e015dc3f752
Hsuan Lee <HsuanGM@gmail.com>
2023-08-23 01:20:58 +0800

    fix(web): missing to set the twitter image (#2752)

--- web/packages/dashboard/src/services/title.service.ts
+++ web/packages/dashboard/src/services/title.service.ts
@@ -4,23 +4,30 @@ import { ReplaySubject } from "rxjs";
 @Injectable()
 export class TitleService {
   title$ = new ReplaySubject<string>(1);
-  setTitle(title: string, ogImageTitle = false) {
+  setTitle(title: string, setToImage = false) {
     const ogTitle = document.querySelector("meta[property='og:title']");
     const ogImage = document.querySelector("meta[property='og:image']");
     const twitterTitle = document.querySelector("meta[name='twitter:title']");
+    const twitterImage = document.querySelector("meta[name='twitter:image']");
+    const ogImageURL = new URL("https://www.lepton.ai/api/og");
     document.title = `${title} | Lepton AI`;
+
+    if (setToImage) {
+      ogImageURL.searchParams.set("title", title);
+    }
+
     if (ogTitle) {
       ogTitle.setAttribute("content", `${title} | Lepton AI`);
     }
     if (twitterTitle) {
       twitterTitle.setAttribute("content", `${title} | Lepton AI`);
     }
+
     if (ogImage) {
-      const ogImageURL = new URL("https://www.lepton.ai/api/og");
-      if (ogImageTitle) {
-        ogImageURL.searchParams.set("title", title);
-      }
       ogImage.setAttribute("content", ogImageURL.toString());
+    }
+    if (twitterImage) {
+      twitterImage.setAttribute("content", ogImageURL.toString());
     }
 
     this.title$.next(title);
