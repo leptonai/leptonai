95ee174ca28d751af291a3a9872dc443f16fc525
Yangqing Jia <jia@lepton.ai>
2023-08-22 19:24:41 -0700

    feat(sdk): bump and mark release 0.8.4 (https://github.com/leptonai/lepton/issues/2759)

--- leptonai/config.py
+++ leptonai/config.py
@@ -19,7 +19,7 @@ DB_PATH = CACHE_DIR / "lepton.db"
 LOGS_DIR = CACHE_DIR / "logs"
 
 # Lepton's base image and image repository location.
-BASE_IMAGE_VERSION = "0.8.0-alpha.2"
+BASE_IMAGE_VERSION = "0.8.4"
 BASE_IMAGE_REGISTRY = "default"
 BASE_IMAGE_REPO = f"{BASE_IMAGE_REGISTRY}/lepton"
 BASE_IMAGE = f"{BASE_IMAGE_REPO}:photon-py{sys.version_info.major}.{sys.version_info.minor}-runner-{BASE_IMAGE_VERSION}"
