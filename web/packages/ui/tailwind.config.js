d93051892535deca6d41f45016fd641e2fec9de3
Hsuan Lee <HsuanGM@gmail.com>
2023-08-28 13:40:19 +0800

    fix(web): fix font-family in `tailwind.config.js` (#2843)

--- web/packages/ui/tailwind.config.js
+++ web/packages/ui/tailwind.config.js
@@ -1,5 +1,3 @@
-const { fontFamily } = require("tailwindcss/defaultTheme");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -49,12 +47,9 @@ module.exports = {
         },
       },
       borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
-      },
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
       keyframes: {
         "accordion-down": {
