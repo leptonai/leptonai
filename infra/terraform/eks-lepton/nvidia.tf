e486846042f96089040774786b11edc8ffbca4d0
Gyuho Lee <6799218+gyuho@users.noreply.github.com>
2023-08-31 17:05:29 +0800

    nits(eks-lepton): document/clean up nvidia gpu-operator helm values (#2906)
    This does not change any value. Same as existing ones. Just document +
    copying default values for later GPU taint (if needed) for satellite
    nodes. Tested in my DEV and works.
    
    Signed-off-by: Gyuho Lee <gyuho@lepton.ai>

--- infra/terraform/eks-lepton/nvidia.tf
+++ infra/terraform/eks-lepton/nvidia.tf
@@ -11,8 +11,8 @@ resource "helm_release" "nvidia_gpu_operator" {
   # version = "v1.0.0-devel"
 
   # https://github.com/NVIDIA/gpu-operator/blob/master/deployments/gpu-operator/values.yaml
-  values = [yamlencode({
-    toolkit = {
+  values = [
+    templatefile("${path.module}/helm/values/nvidia-gpu-operator/defaults.yaml", {
       # this is required for Amazon Linux based AMIs
       # https://github.com/leptonai/lepton/issues/225
       # https://github.com/leptonai/lepton/issues/526
@@ -21,17 +21,15 @@ resource "helm_release" "nvidia_gpu_operator" {
       # Ubuntu is preferred since amzn2 driver is broken
       # errors with Failed to pull image "nvcr.io/nvidia/driver:525.105.17-amzn2"
       # when ami type is "al2"
-      version = var.use_ubuntu_nvidia_gpu_operator ? "v1.13.1-ubuntu20.04" : "v1.13.1-centos7"
-    }
+      toolkit_version = var.use_ubuntu_nvidia_gpu_operator ? "v1.13.1-ubuntu20.04" : "v1.13.1-centos7"
 
-    dcgmExporter = {
-      enabled         = true
-      repository      = "${local.account_id}.dkr.ecr.${var.region}.amazonaws.com"
-      image           = "dcgm-exporter"
-      version         = "3.1.8-3.1.5-ubuntu20.04"
-      imagePullPolicy = "IfNotPresent"
-    }
-  })]
+      # in case "nvcr.io/nvidia/k8s" repository becomes unavailable, we copied the images to our own ECR
+      # also, we DO NOT want to drop some GPU related metrics
+      # so we need to include our own metrics rules for exporter
+      # see https://github.com/leptonai/lepton/pull/1921 for more details
+      dcgm_exporter_container_repo = "${local.account_id}.dkr.ecr.${var.region}.amazonaws.com"
+    }),
+  ]
 
   depends_on = [module.eks]
 }
