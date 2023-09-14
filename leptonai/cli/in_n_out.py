91c6a2791df0f222abc0f00f635db8dc5a47c03d
Yangqing Jia <me@daggerfs.com>
2023-09-13 17:12:07 -0700

    feat(sdk): always create photon when model is specified in lep photon run

--- leptonai/cli/in_n_out.py
+++ leptonai/cli/in_n_out.py
@@ -55,7 +55,7 @@ def cloud_login(credentials=None):
                     "It seems that you are running in a non-GUI environment. You can"
                     " manually obtain credentials from"
                     " [green]https://dashboard.lepton.ai/credentials[/] and copy it"
-                    " over, or use `lep login -c \[credentials]` to log in."
+                    " over, or use `lep login -c \[credentials]` to log in."  # noqa: W605
                 )
             while not credentials:
                 credentials = input("Credential: ")
