4096a15872deb51c4ea70b2be78c94ca1ba18d7c
feifeibear <fangjiarui123@gmail.com>
2023-09-28 02:12:40 +0000

    hotfix version error

--- leptonai/__init__.py
+++ leptonai/__init__.py
@@ -3,4 +3,5 @@ Lepton SDK for Python.
 """
 from . import photon  # noqa: F401
 from .client import Client  # noqa: F401
-from ._version import __version__  # noqa: F401
+
+__all__ = ["Client", "photon"]
