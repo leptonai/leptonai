3be76bf39fc955d6c3265b781320dd00cb2eeee5
Yangqing Jia <me@daggerfs.com>
2023-09-29 23:49:30 -0700

    chore(sdk): clean up prebuilt (#96)

--- leptonai/photon/prebuilt/__init__.py
+++ leptonai/photon/prebuilt/__init__.py
@@ -1,8 +1,4 @@
+# flake8: noqa
 from ..photon import Photon
-from .vllm import vLLM  # noqa: F401
-
-
-class Echo(Photon):
-    @Photon.handler
-    def run(self, input: str) -> str:
-        return input
+from .echo import Echo
+from .vllm import vLLM
