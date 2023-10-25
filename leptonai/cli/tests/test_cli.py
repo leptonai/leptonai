cc2ff5bec1960c9a63e2488d92a77fe4753f8e91
Yangqing Jia <me@daggerfs.com>
2023-10-25 11:26:24 -0700

    feat(sdk): allow uninstalling pip packages

--- leptonai/cli/tests/test_cli.py
+++ leptonai/cli/tests/test_cli.py
@@ -12,6 +12,7 @@ from loguru import logger
 
 from leptonai import config, __version__
 from leptonai.cli import lep as cli
+from leptonai.cli.photon import _sequentialize_pip_commands
 
 
 logger.info(f"Using cache dir: {config.CACHE_DIR}")
@@ -28,6 +29,97 @@ class TestLepCli(unittest.TestCase):
         self.assertEqual(version.exit_code, 0)
         self.assertEqual(v.output.strip(), f"lep, version {__version__}")
         self.assertEqual(version.output.strip(), f"lep, version {__version__}")
+
+
+class TestPipSequences(unittest.TestCase):
+    def test_sequence_correctness(self):
+        sequences = [
+            ["numpy"],
+            ["numpy", "scipy"],
+            ["numpy", "scipy", "uninstall numpy", "torch", "numpy"],
+            ["numpy", "scipy", "uninstall numpy", "uninstall scipy", "torch", "numpy"],
+            [
+                "numpy",
+                "git+http://github.com/leptonai/leptonai/",
+                "uninstall numpy",
+                "scipy",
+                "torch",
+                "numpy",
+                "uninstall scipy",
+            ],
+        ]
+        expected_outputs = [
+            [("install", ["numpy"])],
+            [("install", ["numpy", "scipy"])],
+            [
+                ("install", ["numpy", "scipy"]),
+                ("uninstall", ["numpy"]),
+                ("install", ["torch", "numpy"]),
+            ],
+            [
+                ("install", ["numpy", "scipy"]),
+                ("uninstall", ["numpy", "scipy"]),
+                ("install", ["torch", "numpy"]),
+            ],
+            [
+                ("install", ["numpy", "git+http://github.com/leptonai/leptonai/"]),
+                ("uninstall", ["numpy"]),
+                ("install", ["scipy", "torch", "numpy"]),
+                ("uninstall", ["scipy"]),
+            ],
+        ]
+        for sequence, expected in zip(sequences, expected_outputs):
+            self.assertEqual(
+                _sequentialize_pip_commands(sequence),
+                expected,
+                f"Sequence {sequence} was not parsed correctly",
+            )
+
+    def test_sequence_correctness_edge_cases(self):
+        edge_cases = [
+            [],  # Empty list
+            ["numpy", "scipy", "torch"],  # Only installations
+            ["numpy", "numpy", "scipy", "torch"],  # Duplicated installations
+            [
+                "uninstall numpy",
+                "uninstall scipy",
+                "uninstall torch",
+            ],  # Only uninstallations
+            [
+                "uninstall numpy",
+                "uninstall numpy",
+                "uninstall scipy",
+                "uninstall torch",
+            ],  # Duplicated uninstallations
+            ["numpy", "uninstall scipy", "torch", "uninstall numpy"],  # Interleaved
+            [
+                "numpy_uninstall"
+            ],  # Includes the word uninstall but isn't an uninstall command
+            [
+                "uninstall uninstall numpy"
+            ],  # odd case, whch we don't handle and will interpret this as pip uninstall-ing two packages, named uninstall and numpy.
+        ]
+        expected_outputs = [
+            [],
+            [("install", ["numpy", "scipy", "torch"])],
+            [("install", ["numpy", "scipy", "torch"])],
+            [("uninstall", ["numpy", "scipy", "torch"])],
+            [("uninstall", ["numpy", "scipy", "torch"])],
+            [
+                ("install", ["numpy"]),
+                ("uninstall", ["scipy"]),
+                ("install", ["torch"]),
+                ("uninstall", ["numpy"]),
+            ],
+            [("install", ["numpy_uninstall"])],
+            [("uninstall", ["uninstall numpy"])],
+        ]
+        for sequence, expected in zip(edge_cases, expected_outputs):
+            self.assertEqual(
+                _sequentialize_pip_commands(sequence),
+                expected,
+                f"Sequence {sequence} was not parsed correctly",
+            )
 
 
 if __name__ == "__main__":
