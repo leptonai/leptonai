5d9d2e79946c2f9f303a005c34a35676f88e5b17
Yangqing Jia <me@daggerfs.com>
2023-11-02 11:23:31 -0700

    feat(sdk): utility functions for images

--- leptonai/photon/__init__.py
+++ leptonai/photon/__init__.py
@@ -13,7 +13,7 @@ from .photon import (  # noqa: F401
     FileParam,
     StaticFiles,
 )
-from .types import get_file_content  # noqa: F401
+from .types import get_file_content, make_png_response  # noqa: F401
 import leptonai.photon.hf  # noqa: F401
 import leptonai.photon.vllm  # noqa: F401
 
