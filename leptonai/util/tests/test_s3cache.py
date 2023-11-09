5d3d51e39d7a4259a32c3582a643f87a02ae7971
Yangqing Jia <me@daggerfs.com>
2023-11-09 13:16:45 -0800

    feat(sdk): add S3 file check (#154)

--- leptonai/util/tests/test_s3cache.py
+++ leptonai/util/tests/test_s3cache.py
@@ -25,8 +25,57 @@ class TestS3Cache(unittest.TestCase):
         self.assertTrue(os.path.exists(local_path))
         with open(local_path, "r") as f:
             self.assertEqual(f.read(), "42\n")
+
+        # Modify the local file.
+        with open("/tmp/leptonai-s3cache-test/answer.txt", "w") as f:
+            f.write("1337\n")
+
+        # Test get: should not download the file again if we don't set check_file.
+        local_path = cache.get("answer.txt")
+        self.assertTrue(os.path.exists(local_path))
+        with open(local_path, "r") as f:
+            self.assertEqual(f.read(), "1337\n")
+
+        # Test get: should download the file again if we set check_file.
+        local_path = cache.get("answer.txt", check_file=True)
+        self.assertTrue(os.path.exists(local_path))
+        with open(local_path, "r") as f:
+            self.assertEqual(f.read(), "42\n")
+
+        # Modify the local file but with the same size.
+        with open("/tmp/leptonai-s3cache-test/answer.txt", "w") as f:
+            f.write("43\n")
+
+        # Test get: should not download the file again if we don't set check_file.
+        local_path = cache.get("answer.txt")
+        self.assertTrue(os.path.exists(local_path))
+        with open(local_path, "r") as f:
+            self.assertEqual(f.read(), "43\n")
+
+        # Test get: should download the file again if we set check_file, as it catches file modification time.
+        local_path = cache.get("answer.txt", check_file=True)
+        self.assertTrue(os.path.exists(local_path))
+        with open(local_path, "r") as f:
+            self.assertEqual(f.read(), "42\n")
+
         # Test rsync: rsync the whole thing
         cache.rsync("")
+        self.assertTrue(os.path.exists("/tmp/leptonai-s3cache-test/folder/foo.txt"))
+        with open("/tmp/leptonai-s3cache-test/folder/foo.txt", "r") as f:
+            self.assertEqual(f.read(), "bar\n")
+
+        # Similar to get, we check file modifications
+        with open("/tmp/leptonai-s3cache-test/folder/foo.txt", "w") as f:
+            f.write("baz\n")
+
+        # Test rsync: should not download the file again if we don't set check_file.
+        cache.rsync("")
+        self.assertTrue(os.path.exists("/tmp/leptonai-s3cache-test/folder/foo.txt"))
+        with open("/tmp/leptonai-s3cache-test/folder/foo.txt", "r") as f:
+            self.assertEqual(f.read(), "baz\n")
+
+        # Test rsync: should download the file again if we set check_file.
+        cache.rsync("", check_file=True)
         self.assertTrue(os.path.exists("/tmp/leptonai-s3cache-test/folder/foo.txt"))
         with open("/tmp/leptonai-s3cache-test/folder/foo.txt", "r") as f:
             self.assertEqual(f.read(), "bar\n")
