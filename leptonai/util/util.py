4582df44466a8f2d631231fed01c6175549c7e89
Yangqing Jia <me@daggerfs.com>
2023-11-27 13:49:34 -0800

    fix docstring

--- leptonai/util/util.py
+++ leptonai/util/util.py
@@ -87,16 +87,19 @@ def asyncfy_with_semaphore(
     func, semaphore: Optional[anyio.Semaphore], timeout: Optional[float] = None
 ):
     """Decorator that makes a function async, as well as running in a separate thread,
-    with the concurrency controlled by the semaphore. If the function is already async,
-    this decorator is ignored. If Semaphore is None, we do not enforce an upper bound
-    on the number of concurrent calls (but it is still bound by the number of threads
-    that anyio defines as an upper bound).
+    with the concurrency controlled by the semaphore. If Semaphore is None, we do not
+    enforce an upper bound on the number of concurrent calls (but it is still bound by
+    the number of threads that anyio defines as an upper bound).
 
     Args:
-        func (function): Function to make async
-        semaphore (anyio.Semaphore or None): Semaphore to use for concurrency control. If func
-            is already async, this argument is ignored. If func is sync, it will be
-            guarded by the semaphore.
+        func (function): Function to make async. If the function is already async,
+            this function will add semaphore and timeout control to it.
+        semaphore (anyio.Semaphore or None): Semaphore to use for concurrency control.
+            Concurrent calls to this function will be bounded by the semaphore.
+        timeout (float or None): Timeout in seconds. If the function does not return
+            within the timeout, a TimeoutError will be raised. If None, no timeout
+            will be enforced. If the function is async, one can catch the CancelledError
+            inside the function to handle the timeout.
     """
 
     semaphore_ctx = semaphore if semaphore is not None else nullcontext()
