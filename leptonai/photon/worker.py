d32ace7ebe15c454970ae20339e2e0f1387059a1
Yangqing Jia <me@daggerfs.com>
2023-12-07 18:20:56 -0800

    fix(sdk): update worker's queue, kv to have default names, and deal with init()

--- leptonai/photon/worker.py
+++ leptonai/photon/worker.py
@@ -3,15 +3,17 @@ from abc import abstractmethod
 import datetime
 from enum import Enum
 import json
+import os
 import threading
 import time
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 import uuid
 
 import anyio
 from loguru import logger
 
 from .photon import Photon, HTTPException
+from leptonai.api import workspace as ws_api
 from leptonai.queue import Queue, Empty
 from leptonai.kv import KV
 from leptonai.util import asyncfy_with_semaphore
@@ -25,37 +27,78 @@ class Status(str, Enum):
 
 
 class Worker(Photon):
-    queue_name: str = None
-    kv_name: str = None
+    # The queue name and the KV name that the worker will use. The queue and the KV
+    # will be created if they don't exist. Note that you are responsible for making
+    # sure the uniqueness of queue and kv names. If they are left as None, the deployment
+    # name will serve as the queue name and the kv name.
+    queue_name: Optional[str] = None
+    kv_name: Optional[str] = None
 
+    # Whether to save the result of the task to the KV. If set to False, the result
+    # of on_task will be simply ignored, and only the task state is saved to the KV.
+    # In default, the result is saved to the KV.
+    save_result: bool = True
+
+    # The time to sleep when the queue is empty.
     queue_empty_sleep_time: int = 5
-    save_result: bool = False
 
     # The maximum number of concurrent tasks that the worker can
     # process. Default to 1 to avoid needing to deal with locks.
     worker_max_concurrency: int = 1
 
+    # The tag used to identify the task id in the payload. In most cases, you should
+    # not need to change this.
     LEPTON_TASK_ID_TAG = "_lepton_task_id"
 
+    _loop_started = False
+
     def init(self):
+        """
+        Worker's own init function that starts the loop.
+
+        If you build a derived Photon based on Worker, you can explicitly call
+        super().init() to start the loop in the init function. Note that when the
+        loop starts, the worker will start to process tasks, so any worker specific
+        init (like loading models) should be done before calling super().init().
+
+        If your class does not implement the init function, the Worker's init function
+        will be called automatically.
+        """
         super().init()
+        # Logs in to the workspace if not logged in yet.
+        ws_api.login()
+        # Starts the loop.
+        if not self._loop_started:
+            self._loop_started = True
+            # creates the queue.
+            if self.queue_name is None:
+                if os.environ.get("LEPTON_DEPLOYMENT_NAME") is None:
+                    raise RuntimeError(
+                        "queue_name is not set and LEPTON_DEPLOYMENT_NAME is not set"
+                    )
+                else:
+                    self.queue_name = os.environ["LEPTON_DEPLOYMENT_NAME"]
+            self._queue = Queue(
+                self.queue_name, create_if_not_exists=True, wait_for_creation=True
+            )
+            # creates the kv.
+            if self.kv_name is None:
+                if os.environ.get("LEPTON_DEPLOYMENT_NAME") is None:
+                    raise RuntimeError(
+                        "kv_name is not set and LEPTON_DEPLOYMENT_NAME is not set"
+                    )
+                else:
+                    self.kv_name = os.environ["LEPTON_DEPLOYMENT_NAME"]
+            self._kv = KV(
+                self.kv_name, create_if_not_exists=True, wait_for_creation=True
+            )
 
-        if self.queue_name is None:
-            raise RuntimeError("queue_name is not set")
-        self._queue = Queue(
-            self.queue_name, create_if_not_exists=False, wait_for_creation=True
-        )
-        if self.kv_name is None:
-            raise RuntimeError("kv_name is not set")
-        self._kv = KV(self.kv_name, create_if_not_exists=False, wait_for_creation=True)
-
-        self._worker_semaphore = anyio.Semaphore(self.worker_max_concurrency)
-        self._on_task_handler = asyncfy_with_semaphore(
-            self.on_task, self._worker_semaphore
-        )
-
-        self._lepton_worker_thread_should_exit = False
-        threading.Thread(target=self._worker_loop, daemon=True).start()
+            self._worker_semaphore = anyio.Semaphore(self.worker_max_concurrency)
+            self._on_task_handler = asyncfy_with_semaphore(
+                self.on_task, self._worker_semaphore
+            )
+            self._lepton_worker_thread_should_exit = False
+            threading.Thread(target=self._worker_loop, daemon=True).start()
 
     def _handle_exit(self, *args, **kwargs) -> None:
         self._lepton_worker_thread_should_exit = True
@@ -63,7 +106,6 @@ class Worker(Photon):
 
     def _worker_loop(self):
         """Keep polling the queue for new tasks, and dispatch them to `on_task` (through `_on_task`)"""
-
         loop = asyncio.new_event_loop()
 
         while True:
@@ -86,7 +128,6 @@ class Worker(Photon):
 
     async def _on_task(self, task_id: str, **payload):
         """Do some accounting and error handling work, then dispatch the task to `on_task`"""
-
         logger.info(f"Received task {task_id}")
 
         task_json = json.loads(self._kv.get(task_id))
