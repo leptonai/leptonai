29dac62d2a15efe2f0b7c72bae35ef8163cf3a08
bddppq <jbai@lepton.ai>
2024-01-02 20:18:52 -0800

    add openssh server to base image (#251)

--- leptonai/photon/dockerfiles/install_base.sh
+++ leptonai/photon/dockerfiles/install_base.sh
@@ -3,4 +3,4 @@
 set -e
 
 apt-get update
-apt-get install -y sudo software-properties-common build-essential tzdata git
+apt-get install -y sudo software-properties-common build-essential tzdata git openssh-server
