3214188f96545d8c34f64d8d9ed760e842082846
Yangqing Jia <me@daggerfs.com>
2024-03-24 01:16:35 -0700

    feat(sdk): add universal tokenizer readme

---
+++ leptonai/templates/universal-tokenizer/__init__.py
@@ -0,0 +1,6 @@
+# flakes8: noqa
+"""
+The universal tokenizer photon is available on the Lepton AI platform. This is a stub to store the README file for the photon.
+"""
+
+_photon_name = "universal_tokenizer"
