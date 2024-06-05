ac8e60e212365dc572b3223b4241f492218baf28
Yangqing Jia <me@daggerfs.com>
2024-06-04 22:17:26 -0700

    feat[cli]: move cli login/logout/workspace to new v1 api (#400)
    * move cli login/logout/workspace to new v1 api
    
    * linkt

--- leptonai/cli/cli.py
+++ leptonai/cli/cli.py
@@ -1,8 +1,12 @@
 import click
+import sys
+import webbrowser
+
+from .util import console
+from leptonai.api.v1.workspace_record import WorkspaceRecord
 
 import leptonai
 from . import deployment
-from . import in_n_out
 from . import job
 from . import kv
 from . import objectstore
@@ -15,6 +19,15 @@ from . import workspace
 from .util import click_group
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+LOGIN_LOGO = """\
+========================================================
+[blue]    _     _____ ____ _____ ___  _   _       _    ___     [/]
+[blue]   | |   | ____|  _ \\_   _/ _ \\| \\ | |     / \\  |_ _|    [/]
+[blue]   | |   |  _| | |_) || || | | |  \\| |    / _ \\  | |     [/]
+[blue]   | |___| |___|  __/ | || |_| | |\\  |   / ___ \\ | |     [/]
+[blue]   |_____|_____|_|    |_| \\___/|_| \\_|  /_/   \\_\\___|    [/]
+
+========================================================"""
 
 
 @click.version_option(leptonai.__version__, "-v", "--version")
@@ -54,11 +67,83 @@ workspace.add_command(lep)
 def login(credentials):
     """
     Login to the Lepton AI cloud. This will open a browser window to the Lepton AI
-    login page. After logging in, you will be redirected to a page with an access
-    code. Copy the access code and paste it into the terminal, and you will be
-    logged in.
+    login page if credentials are not given. You will be redirected to a page with
+    the credentials string. Copy the string and paste it into the terminal, and
+    you will be logged in.
     """
-    in_n_out.cloud_login(credentials=credentials)
+    console.print(LOGIN_LOGO)
+    need_further_login = False
+    if credentials:
+        workspace_id, auth_token = credentials.split(":", 1)
+        WorkspaceRecord.set(workspace_id, auth_token=auth_token)
+    else:
+        if WorkspaceRecord.current():
+            # Already logged in. Notify the user the login status.
+            pass
+        else:
+            candidates = WorkspaceRecord.workspaces()
+            if len(candidates) == 0:
+                need_further_login = True
+            elif len(candidates) == 1:
+                # Only one workspace, so we will simply log in to that one.
+                ws = candidates[0]
+                WorkspaceRecord.set(ws.id_, ws.auth_token, ws.url)  # type: ignore
+            else:
+                # multiple workspaces. login to one of them.
+                console.print("You have multiple workspaces. Please select one:")
+                for i, ws in enumerate(candidates):
+                    console.print(f"{i+1}. {ws.id_} ({ws.display_name})")
+                choice = None
+                while not choice:
+                    choice = input("choice: ")
+                try:
+                    choice = int(choice) - 1
+                except ValueError:
+                    console.print("Invalid choice. Please enter a number.")
+                    sys.exit(1)
+                WorkspaceRecord.set(
+                    candidates[choice].id_,  # type: ignore
+                    candidates[choice].auth_token,
+                    candidates[choice].url,
+                )
+                console.print(
+                    "Hint: If you have multiple workspaces, you can pick the one you"
+                    " want\nto log in via `lep workspace login -i workspace_id`."
+                )
+    if need_further_login:
+        # there is no credentials, and no current workspace. Will need to ask the
+        # user to login interactively.
+        # obtain credentials first.
+        console.print(
+            "Welcome to Lepton AI. We will open a browser for you to obtain your"
+            " login credentials. Please log in with your registered account."
+        )
+        console.print(
+            "You'll then be presented with your CLI credentials. If you have"
+            " multiple workspaces, there will be multiple credentials - select the"
+            " one you want to log in to. Copy the credential and paste it here."
+        )
+        input("Whenever you are ready, press Enter to continue...")
+
+        success = webbrowser.open("https://dashboard.lepton.ai/credentials")
+        if not success:
+            console.print(
+                "It seems that you are running in a non-GUI environment. You can"
+                " manually obtain credentials from"
+                " [green]https://dashboard.lepton.ai/credentials[/] and copy it"
+                " over, or use `lep login -c \[credentials]` to log in."  # noqa: W605
+            )
+        while not credentials:
+            credentials = input("Credential: ")
+        workspace_id, auth_token = credentials.split(":", 1)
+        WorkspaceRecord.set(workspace_id, auth_token=auth_token)
+    # Try to login and print the info.
+    api_client = WorkspaceRecord.client()
+    info = api_client.info()
+    console.print(f"Logged in to your workspace [green]{info.workspace_name}[/].")
+    console.print(f"\t      tier: {info.workspace_tier}")
+    console.print(f"\tbuild time: {info.build_time}")
+    console.print(f"\t   version: {api_client.version()}")
 
 
 @lep.command()
@@ -69,7 +154,8 @@ def logout(purge):
     """
     Logout of the Lepton AI cloud.
     """
-    in_n_out.cloud_logout(purge=purge)
+    WorkspaceRecord.logout(purge=purge)
+    console.print("[green]Logged out[/]")
 
 
 if __name__ == "__main__":
