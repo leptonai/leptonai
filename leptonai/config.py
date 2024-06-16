81f669c66cb67f382ac0d0d9c8a258c04d8ed4e3
Yangqing Jia <me@daggerfs.com>
2024-06-16 02:30:08 -0700

    release: 0.21.1 (#414)

--- leptonai/config.py
+++ leptonai/config.py
@@ -215,7 +215,7 @@ def _is_rocm() -> bool:
 
 
 # Lepton's base image and image repository location.
-BASE_IMAGE_VERSION = "0.21.0"
+BASE_IMAGE_VERSION = "0.21.1"
 BASE_IMAGE_REGISTRY = "default"
 BASE_IMAGE_REPO = f"{BASE_IMAGE_REGISTRY}/lepton"
 BASE_IMAGE = (
