a9d59c5f35060dc6f5c81a331e27987dbc245294
xlu451 <xlu@lepton.ai>
2024-07-18 14:34:30 -0700

    Add multiple ng help info

--- leptonai/cli/pod.py
+++ leptonai/cli/pod.py
@@ -91,7 +91,8 @@ def pod():
     "--node-group",
     "-ng",
     "node_groups",
-    help="Node group for the job. If not set, use on-demand resources.",
+    help="Node group for the pod. If not set, use on-demand resources."
+         " You can repeat this flag multiple times to choose multiple node groups.",
     type=str,
     multiple=True,
 )
