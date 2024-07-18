9d9bc8f9e548a1d78ac6b5ca2247555020c8539f
xlu451 <xlu@lepton.ai>
2024-07-18 15:33:18 -0700

    Remove the declaration of deployment_template

--- leptonai/cli/deployment.py
+++ leptonai/cli/deployment.py
@@ -316,7 +316,6 @@ def create(
 
     # First, check whether the input is photon or container. We will prioritize using
     # photon if both are specified.
-    deployment_template = PhotonDeploymentTemplate()
     if photon_name is not None or photon_id is not None:
         # We will use photon.
         if container_image is not None or container_command is not None:
