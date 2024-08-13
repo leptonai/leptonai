d089ef1407261aa3473b08837277959d6cdd9e95
Xiangyu Lu <xiangyulu@Xiangyus-MBP.attlocal.net>
2024-08-13 13:59:54 -0700

    fix public_photon flag

--- leptonai/cli/deployment.py
+++ leptonai/cli/deployment.py
@@ -1108,7 +1108,7 @@ def log(name, replica):
         "If specified, get the photon from the public photon registry. If not"
         " specified, we will inherit the namespace of the current deployment."
     ),
-    default=None,
+    default=False,
 )
 @click.option(
     "--visibility",
@@ -1212,7 +1212,7 @@ def update(
     if id == "latest":
         lepton_deployment = client.deployment.get(name)
         current_photon_id = lepton_deployment.spec.photon_id
-        photons = client.photon.list_all()
+        photons = client.photon.list_all(public_photon)
 
         for photon in photons:
             if photon.id_ == current_photon_id:
@@ -1231,12 +1231,6 @@ def update(
         ]
         id = sorted(records, key=lambda x: x[3])[-1][2]  # type: ignore
         console.print(f"Updating to latest photon id [green]{id}[/].")
-    if public_photon is None:
-        lepton_deployment = client.deployment.get(name)
-
-        public_photon = (
-            lepton_deployment.spec.photon_namespace or "private"
-        ) == "public"
     if remove_tokens:
         # [] means removing all tokens
         tokens = []
