8faf8754e70819fdfa412678f21335b03bd5fce0
Xiangyu Lu <169013972+xlu451@users.noreply.github.com>
2024-08-19 23:58:53 -0700

    fix: photon multiple values for argument request error (#470)
    * fix/photon-multiple-values-for-argument-request-error
    
    * lint
    
    ---------
    
    Co-authored-by: Xiangyu Lu <xiangyulu@Xiangyus-MBP.attlocal.net>

--- leptonai/photon/photon.py
+++ leptonai/photon/photon.py
@@ -52,6 +52,7 @@ from leptonai.config import (  # noqa: F401
     PYDANTIC_MAJOR_VERSION,
     VALID_SHAPES,
     get_local_deployment_token,
+    PHOTON_FORBIDDEN_PARAMETER_NAMES,
 )
 from leptonai.photon.constants import METADATA_VCS_URL_KEY
 from leptonai.photon.download import fetch_code_from_vcs
@@ -1235,6 +1236,12 @@ class Photon(BasePhoton):
 
     @staticmethod
     def handler(path=None, method="POST", **kwargs):
+        for key in PHOTON_FORBIDDEN_PARAMETER_NAMES:
+            if key in kwargs:
+                raise ValueError(
+                    f"The parameter name '{key}' is not allowed. "
+                    "Please rename the parameter and rebuild Photon."
+                )
         Photon._santity_check_handler_kwargs(kwargs)
 
         def decorator(func):
