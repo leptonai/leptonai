1363aa32697c891ad499f7e741c2762a7b89bcc9
xlu451 <xlu@lepton.ai>
2024-09-05 15:44:10 -0700

    Remove duplicate api/v1/storage function delete_file_or_dir

--- leptonai/api/v1/storage.py
+++ leptonai/api/v1/storage.py
@@ -99,14 +99,6 @@ class StorageAPI(APIResourse):
         return self.ensure_ok(response)
 
     def delete_file_or_dir(
-        self, additional_path: str, file_system: Optional[str] = None
-    ) -> bool:
-        file_system = file_system or DEFAULT_STORAGE_VOLUME_NAME
-        response = self._delete(
-            f"/storage/{file_system}{_prepend_separator(additional_path)}"
-        )
-
-    def delete_file_or_dir(
         self, additional_path: str, delete_all=False, file_system: Optional[str] = None
     ) -> bool:
         file_system = file_system or DEFAULT_STORAGE_VOLUME_NAME
