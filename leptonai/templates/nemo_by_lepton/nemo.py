7c4ec8b16ebedbbd4d6ff29ba7c8f2a3ef768934
Ken D <Ken Ding>
2024-09-25 04:25:46 +0000

    minor changes

--- leptonai/templates/nemo_by_lepton/nemo.py
+++ leptonai/templates/nemo_by_lepton/nemo.py
@@ -5,7 +5,7 @@ from fastapi import UploadFile, File
 
 class Transcriber(Photon):
     # The init method implements any custom initialization logic we need.
-    requirement_dependency = ["Cython", "packaging", "numpy==1.26.3", "nemo_toolkit[all]", "huggingface-hub==0.23.2"]
+    requirement_dependency = ["Cython", "packaging", "nemo_toolkit[all]", "huggingface-hub==0.23.2", "numpy==1.26.3",]
     system_dependency = ["libsndfile1", "ffmpeg"]
 
     def init(self):
@@ -13,7 +13,7 @@ class Transcriber(Photon):
         self.asr_model = EncDecMultiTaskModel.from_pretrained('nvidia/canary-1b')
 
     # When no name is specified, the handler name is the method name.
-    @Photon.handler("/transcribe_audio", method="POST", use_raw_args=True)
+    @Photon.handler("/transcribe_audio/", method="POST", use_raw_args=True)
     async def transcribe_audio(self, file: UploadFile = File(...)):
         # Save the uploaded file to a temporary file
         with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp:
