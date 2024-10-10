88c3e4e88049fb6d1f525a851f2fcefb7e8e1350
xlu451 <xlu@lepton.ai>
2024-10-09 17:21:25 -0700

    fix client wrong deployment url

--- leptonai/client.py
+++ leptonai/client.py
@@ -305,7 +305,7 @@ class Client(object):
                         f" url returned: {url}. Please report this as a bug."
                     )
                 self.url = (
-                    url[:insert_index] + "-" + deployment + url[insert_index:]
+                    url[:insert_index] + "-" + deployment + '.tin.lepton.run'
                 ).rstrip("/")
 
         headers = {}
