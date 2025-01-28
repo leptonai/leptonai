44e0865bdff04b2cbbd28795d0d7ebeb507267cb
xlu451 <xlu@lepton.ai>
2025-01-27 17:28:51 -0800

    fix env error

--- leptonai/api/v1/photon.py
+++ leptonai/api/v1/photon.py
@@ -31,7 +31,7 @@ def make_mounts_from_strings(
         if len(parts) == 2:
             # TODO: sanity check if the mount path exists.
             mount_list.append(Mount(path=parts[0].strip(), mount_path=parts[1].strip()))  # type: ignore
-        if len(parts) == 3:
+        elif len(parts) == 3:
             mount_list.append(
                 Mount(
                     path=parts[0].strip(),
