8730e06bae548602ee5348051459e3f611dfa019
xiangyu_lu <xiangyul@nvidia.com>
2025-08-06 01:43:39 -0700

    update

--- leptonai/cli/cli.py
+++ leptonai/cli/cli.py
@@ -14,13 +14,8 @@ from loguru import logger
 import leptonai
 from . import deployment, node
 from . import job
-from . import kv
-from . import objectstore
-from . import photon
 from . import pod
-from . import queue
 from . import secret
-from . import storage
 from . import workspace
 from . import ingress
 from . import log
@@ -65,13 +60,8 @@ def lep():
 # Add subcommands
 deployment.add_command(lep)
 job.add_command(lep)
-# kv.add_command(lep)
-# objectstore.add_command(lep)
-# photon.add_command(lep)
 pod.add_command(lep)
-# queue.add_command(lep)
 secret.add_command(lep)
-# storage.add_command(lep)
 workspace.add_command(lep)
 node.add_command(lep)
 
