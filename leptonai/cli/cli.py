82acdeb6207707fa9a520af14877992938a321cf
Xiangyu Lu <169013972+xlu451@users.noreply.github.com>
2025-09-02 21:13:50 -0700

    Revert "feat: Add LeptonRayCluster Spec and Operations (#659)"
    This reverts commit 4cd3c92741ebb27c4bd622096b367d693bc97b5a.

--- leptonai/cli/cli.py
+++ leptonai/cli/cli.py
@@ -28,7 +28,6 @@ from . import storage
 from . import workspace
 from . import ingress
 from . import log
-from . import raycluster
 
 from .util import click_group
 
@@ -80,7 +79,6 @@ node.add_command(lep)
 
 ingress.add_command(lep)
 log.add_command(lep)
-raycluster.add_command(lep)
 
 
 @lep.command()
