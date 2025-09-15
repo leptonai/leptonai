1079d7bde24b4e2d65159afff9253f25f03865b3
xiangyu_lu <xiangyul@nvidia.com>
2025-09-15 14:54:26 -0700

    fix test_client_timeout_streaming

--- leptonai/photon/tests/test_client.py
+++ leptonai/photon/tests/test_client.py
@@ -381,7 +381,8 @@ class TestClientTimeout(unittest.TestCase):
 
         # timeout
         proc, port = photon_run_local_server_simple(StreamingLongPhoton)
-        client = Client(local(port=port), timeout=0.5)
+        client = Client(local(port=port),
+                    timeout=httpx.Timeout(read=0.2, connect=5.0, write=5.0))
         with self.assertRaises(httpx.ReadTimeout):
             result = client.run()
             for _ in result:
