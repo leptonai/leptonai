666f82dbdeef3732ba3ea264d19c3270d38f3e5f
xiangyu_lu <xiangyul@nvidia.com>
2025-10-15 10:56:31 -0700

    lint

--- leptonai/cli/job.py
+++ leptonai/cli/job.py
@@ -765,7 +765,7 @@ def create(
     logger.trace(json.dumps(job.model_dump(), indent=2))
     # Create job and display success message
     created_job = client.job.create(job)
-    logger.trace(f"created_job: ===========")
+    logger.trace("created_job: ===========")
     logger.trace(json.dumps(created_job.model_dump(), indent=2))
     new_job_id = created_job.metadata.id_
     console.print(
