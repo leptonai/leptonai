24c5dc23c180b6378569c20d6aab57c1a2909eb9
xiangyu_lu <xiangyul@nvidia.com>
2026-01-15 13:21:35 -0800

    feat: support resource shape query by  purpose

--- leptonai/api/v2/resource_shape.py
+++ leptonai/api/v2/resource_shape.py
@@ -4,11 +4,14 @@ from .types.shape import Shape
 
 
 class ResourceShapeAPI(APIResourse):
-    def list_shapes(self, node_group: Optional[str] = None) -> List[Shape]:
+    def list_shapes(
+        self, node_group: Optional[str] = None, purpose: Optional[str] = None
+    ) -> List[Shape]:
         """List resource shapes.
 
         Args:
             node_group: Optional node group name filter.
+            purpose: Optional purpose filter (e.g. deployment|pod|job).
 
         Returns:
             List[Shape]: Available resource shapes.
@@ -16,6 +19,8 @@ class ResourceShapeAPI(APIResourse):
         params = {}
         if node_group:
             params["node_groups"] = node_group
+        if purpose:
+            params["purpose"] = purpose
 
         response = self._get("/shapes", params=params)
         return self.ensure_list(response, Shape)
